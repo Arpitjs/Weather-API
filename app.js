0d3c292e2d7e4b2a274d1275b329be68a1977878
Arpit Satyal <arpited7@gmail.com>
2019-09-30 10:26:28 +0545

    weather and geocode apis

--- app.js
+++ app.js
@@ -14,17 +14,20 @@ request({ url: url, json: true }, (err, response) => {
 
 //geocoding
 //adrress -> lat/long -> weather
-let geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/kathmandu.json?access_token=pk.eyJ1IjoiYXJwaXRqcyIsImEiOiJjazE1dWsxbzQweXc0M2xxZGtreHloZmZpIn0.ZsPq84XxmY1s6vOC_D-yxw&limit=1'
-request({ url: geoCodeUrl, json: true }, (err, response) => {
-    if (err) {
-        console.log('cannot connect to the mapbox API.')
-    } else if(response.body.features.length === 0) {
-            console.log('no such place exists.')
-    } 
-    else {
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-        console.log(`the longitude and latitude of ${response.body.features[0].text} are ${latitude} and ${longitude} respectively.`)
-    }
-
-})
+function geoCodes(place) {
+    let geoCodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=pk.eyJ1IjoiYXJwaXRqcyIsImEiOiJjazE1dWsxbzQweXc0M2xxZGtreHloZmZpIn0.ZsPq84XxmY1s6vOC_D-yxw&limit=1`
+    request({ url: geoCodeUrl, json: true }, (err, response) => {
+        if (err) {
+            console.log('cannot connect to the mapbox API.')
+        } else if(response.body.features.length === 0) {
+                console.log('no such place exists.')
+        } 
+        else {
+            const latitude = response.body.features[0].center[0]
+            const longitude = response.body.features[0].center[1]
+            console.log(`the longitude and latitude of ${response.body.features[0].text} are ${latitude} and ${longitude} respectively.`)
+        }
+    
+    })
+}
+geoCodes('kathmandu')
